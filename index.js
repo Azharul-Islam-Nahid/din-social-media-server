a82282bc3e81430ee4519d2cce744f8568be505b
Azharul Islam <lonewolfazharul@gmail.com>
2022-12-28 22:31:23 +0600

    connected mongodb created user post & get api

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ktpwcuu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+        const usersCollection = client.db('dinSocialMedia').collection('users');
+
+
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+        app.get('/user', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const userInfo = await usersCollection.find(query).toArray();
+            res.send(userInfo);
+        })
+
+
+    }
+
+    finally {
+
+    }
+}
+
+run().catch(console.log);
+
+
 
 app.get('/', (req, res) => {
     res.send('Din social media server running');
